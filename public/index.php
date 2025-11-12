a3f69b379c77c6cc477f02aa5406057549c64adc
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-11-12 22:32:35 +0700

    feat(public): add public asset

--- public/index.php
+++ public/index.php
@@ -5,16 +5,28 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Define the base path for the application, so that we can use it in the future (e.g. for deployment)
+$BASE_PATH = __DIR__.'/..';
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+// Define the request URI
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Check if environment file is missing
+if (!file_exists($BASE_PATH.'/.env') && strpos($requestUri, 'pre-setup') === false) {
+    header('Location: /pre-setup.html');
+    exit;
+}
+
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $BASE_PATH.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $BASE_PATH.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
