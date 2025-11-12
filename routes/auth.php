3759fe3274e3a1bb910c27acfb232ff2e41463c8
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-11-12 22:32:20 +0700

    feat(route): add auth route and clean up web route

---
+++ routes/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('web')->group(function () {
+    /*
+    |--------------------------------------------------------------------------
+    | Unauthenticated Route
+    |--------------------------------------------------------------------------
+    |
+    | You can list public endpoint for any user in here. These routes are meant
+    | to be used for guests and are not guarded by any authentication system.
+    | Remember not to list anything of importance, use authenticate route instead.
+    */
+
+    // TODO: Add your unauthenticated routes here
+});
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Public Route
+|--------------------------------------------------------------------------
+|
+| You can list public endpoint for any user in here. These routes are not guarded
+| by any authentication system. In other words, any user can access it directly.
+| Remember not to list anything of importance, use authenticate route instead.
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing.home');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Authenticated Route
+|--------------------------------------------------------------------------
+|
+| In here you can list any route for authenticated user. These routes
+| are meant to be used privately since the access is exclusive to authenticated
+| user who had obtained their access through the login process.
+*/
