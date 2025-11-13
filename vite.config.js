2ec1dc27ebe781e0f353a9a070ce0bf1826cee36
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-11-14 02:19:30 +0700

    chore(vite): update vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,36 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/js/app.js',
+
                 'resources/css/landing.css',
-                'resources/js/app.js'
+
+                'resources/js/addon/home-page.js',
+                'resources/js/addon/layout-dashboard.js',
+                'resources/js/addon/layout-error.js',
+                'resources/js/addon/signin-page.js',
+                'resources/js/addon/timeline-page.js',
+
+                'resources/js/lib/boxicons.js',
+                'resources/js/lib/apexchart.js',
             ],
             refresh: true,
         }),
-        tailwindcss(),
+        tailwindcss({
+            optimize: {
+                minify: true,
+            }
+        }),
     ],
+    build: {
+        chunkSizeWarningLimit: 600,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    sweetalert2: ['sweetalert2'],
+                    axios: ['axios'],
+                }
+            },
+        },
+    },
 });
