54f0285d0744f149136c330ba6742378b4d525af
Apri Pandu W <sarahpalastrin@gmail.com>
2025-11-23 14:14:33 +0700

    chore(lang): update email footer text for both lang

--- lang/en/email.php
+++ lang/en/email.php
@@ -3,6 +3,6 @@
 return [
 
     'regards' => 'Regards,',
-    'footer' => 'If you\'re having trouble clicking the :actionText button, copy and paste the URL below\\n into your web browser: ',
+    'footer' => 'If you\'re having trouble clicking the :actionText button, copy and paste the URL below into your web browser: ',
 
 ];
--- lang/id/email.php
+++ lang/id/email.php
@@ -3,6 +3,6 @@
 return [
 
     'regards' => 'Salam Hormat,',
-    'footer' => 'Jika Anda mengalami masalah saat mengklik tombol :actionText, salin dan tempel URL di bawah ini\n ke peramban web Anda: ',
+    'footer' => 'Jika Anda mengalami masalah saat mengklik tombol :actionText, salin dan tempel URL di bawah ini ke peramban web Anda: ',
 
 ];
